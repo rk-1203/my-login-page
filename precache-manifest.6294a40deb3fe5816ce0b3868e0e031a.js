a03859b925620cd167ac401c8039d2a320677b80
rk-1203 <2015ucp1727@mnit.ac>
2020-01-11 15:03:04 +0530

    Updates

---
+++ precache-manifest.6294a40deb3fe5816ce0b3868e0e031a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecba94604fd360df046514acfed241a3",
+    "url": "/my-login-page/index.html"
+  },
+  {
+    "revision": "e305451e7fdfbdf679a5",
+    "url": "/my-login-page/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2aa12697cf3bd071674f",
+    "url": "/my-login-page/static/js/2.a3a433f7.chunk.js"
+  },
+  {
+    "revision": "e305451e7fdfbdf679a5",
+    "url": "/my-login-page/static/js/main.5d11895e.chunk.js"
+  },
+  {
+    "revision": "8e9fc1e273aa5064e35b",
+    "url": "/my-login-page/static/js/runtime~main.3a316a00.js"
+  },
+  {
+    "revision": "ebffb57dd93964aa60f63829e8b56cfc",
+    "url": "/my-login-page/static/media/Loader.ebffb57d.gif"
+  }
+]);
